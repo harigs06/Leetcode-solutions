d9a6b9d871ba4db9700f0d3e9406f1f0386e7d9e
Hari Ganga Sai <harigangamitta@gmail.com>
2026-08-01 20:38:58 +0530

    Time: 66 ms (88.14%), Space: 47.8 MB (67.5%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
+        
+        if (!set.contains(endWord)) {
+            return 0;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                String currentWord = queue.poll();
+
+                if (currentWord.equals(endWord)) {
+                    return level;
+                }
+
+                char[] wordChars = currentWord.toCharArray();
+                for (int j = 0; j < wordChars.length; j++) {
+                    char originalChar = wordChars[j];
+
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == originalChar) continue;
+
+                        wordChars[j] = c;
+                        String newWord = new String(wordChars);
+
+                        if (set.contains(newWord)) {
+                            queue.add(newWord);
+                            set.remove(newWord);
+                        }
+                    }
+
+                    wordChars[j] = originalChar;
+                }
+            }
+            level++;
+        }
+
+        return 0;
+    }
+}
