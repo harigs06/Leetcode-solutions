ffe232d5cafce9fc774e25ea008dd27598bfb488
Hari Ganga Sai <harigangamitta@gmail.com>
2026-08-17 11:38:31 +0530

    Time: 4 ms (91%), Space: 66.3 MB (48.66%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,65 +1,46 @@
 class DisjointSet {
-    List<Integer> size;
-    List<Integer> rank;
-    List<Integer> parent;
+    private int[] parent;
+    private int[] rank;
     int extras;
-    int n;
+    int components;
 
-    // Use n + 1 if you want 1-based indexing support out of the box
     public DisjointSet(int n) {
-        extras = 0;
-        size = new ArrayList<>();
-        rank = new ArrayList<>();
-        parent = new ArrayList<>();
-        this.n = n;
-        
-        for (int i = 0; i <= n; i++) {
-            size.add(1);
-            rank.add(0);
-            parent.add(i);
+        this.extras = 0;
+        this.components = n; 
+        this.parent = new int[n];
+        this.rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
     }
 
     public int findP(int u) {
-        if (u == parent.get(u)) return u;
-        parent.set(u, findP(parent.get(u)));
-        return parent.get(u);
-    }
-
-    public boolean find(int u, int v) {
-        return findP(u) == findP(v);
+        if (u == parent[u]) return u;
+        return parent[u] = findP(parent[u]); 
     }
 
     public void unionByRank(int u, int v) {
         int ulp_u = findP(u);
         int ulp_v = findP(v);
 
-        if (ulp_u == ulp_v){
-             extras++;
-             return;
-            }
+        if (ulp_u == ulp_v) {
+            extras++;
+            return;
+        }
 
-        int ru = rank.get(ulp_u);
-        int rv = rank.get(ulp_v);
+        components--;
 
-        if (ru < rv) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rv < ru) {
-            parent.set(ulp_v, ulp_u);
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
         } else {
-            parent.set(ulp_u, ulp_v);
-            rank.set(ulp_v, rank.get(ulp_v) + 1);
+            parent[ulp_u] = ulp_v;
+            rank[ulp_v]++;
         }
     }
-
-    public int components() {
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (parent.get(i) == i) count++;
-        }
-        return count;
-    }
-  }  
+}
 class Solution {
     public int makeConnected(int n, int[][] connections) {
 
@@ -68,8 +49,8 @@ class Solution {
             ds.unionByRank(edge[0],edge[1]);
         }
 
-        int components = ds.components();
-        int edgesNeeded = components - 1;
+        
+        int edgesNeeded = ds.components - 1;
 
         return ds.extras >= edgesNeeded ? edgesNeeded : -1;
 
