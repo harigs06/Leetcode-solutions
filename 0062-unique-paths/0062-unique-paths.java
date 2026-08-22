ff9eeea463812b4495012f700af5c330c8eeed41
Hari Ganga Sai <harigangamitta@gmail.com>
2026-08-22 11:46:21 +0530

    Time: 0 ms (100%), Space: 42 MB (82.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,14 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                dp[j] += dp[j - 1];
-            }
+        int totalSteps = m + n - 2;
+        int k = Math.min(m - 1, n - 1); 
+        long ans = 1;
+        
+        for (int i = 1; i <= k; i++) {
+            ans = ans * (totalSteps - k + i) / i;
         }
-
-        return dp[n - 1];
+        
+        return (int) ans;
     }
 }
